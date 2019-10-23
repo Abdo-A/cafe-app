6af1f5e6d06764ae5bd4c1b03076ac8efd4f90c7
Abdo <abdopoetry@gmail.com>
2019-10-23 20:50:33 +0200

    Build Menu page structure

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,21 @@
 import { Card } from 'antd';
+import { useHistory } from 'react-router-dom';
 import React from 'react';
 
 import './style.css';
 
-const Header = () => (
-  <Card>
-    <p className="header__title">Cafe React</p>
-  </Card>
-  );
+const Header = () => {
+  const history = useHistory();
+
+  const onClick = () => {
+    history.push('/');
+  };
+
+  return (
+    <Card hoverable onClick={onClick}>
+      <p className="header__title">Cafe React</p>
+    </Card>
+    );
+};
 
 export default Header;
--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,20 +1,38 @@
+import { Button } from 'antd';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import MenuItem from './components/MenuItem';
 
 import './style.css';
 
-const Menu = () => (
-  <div
-    className="menu__container"
-  >
-    <MenuItem item={{}} />
-    <MenuItem item={{}} />
-    <MenuItem item={{}} />
-    <MenuItem item={{}} />
-    <MenuItem item={{}} />
-    <MenuItem item={{}} />
-  </div>
-);
+const Menu = ({ history }) => {
+  const onRedirectToAddItem = () => {
+    history.push('/add');
+  };
+  return (
+    <div>
+      <div>
+        <div className="menu__upperRowContainer">
+          <p className="menu__upperRowContainer__identifier">Menu</p>
+          <Button onClick={onRedirectToAddItem} type="primary">Add menu item</Button>
+        </div>
+        <div className="menu__itemsContainer">
+          <MenuItem item={{}} />
+          <MenuItem item={{}} />
+          <MenuItem item={{}} />
+          <MenuItem item={{}} />
+          <MenuItem item={{}} />
+          <MenuItem item={{}} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Menu.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
 
 export default Menu;
