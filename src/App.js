1dfffac474bbe23eaeb7eab629b1a4e042e267f1
Abdo <abdopoetry@gmail.com>
2019-10-23 21:16:28 +0200

    Build AddMenuItem page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ import AddMenuItem from './pages/AddMenuItem';
 import Header from './components/Header';
 import Menu from './pages/Menu';
 
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
       <Switch>
         <Route path="/" exact component={Menu} />
         <Route path="/add" component={AddMenuItem} />
         <Redirect to={{ component: Menu }} />
       </Switch>
-    </div>
+    </>
   );
 }
 
--- src/pages/AddMenuItem/index.js
+++ src/pages/AddMenuItem/index.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import {
+ Button, Input, InputNumber, Select,
+} from 'antd';
+import React, { useState } from 'react';
 
-const AddMenuItem = () => (
-  <h4>Add Menu Item</h4>
+import './style.css';
+
+const { Option } = Select;
+
+const AddMenuItem = () => {
+  const [item, setItem] = useState({ type: '', name: '', price: '' });
+
+  const updateItem = (key, value) => {
+    setItem({ ...item, [key]: value });
+  };
+
+  // Check that all inputs are filled by user
+  const isSaveButtonEnabled = Object.values(item).every(Boolean);
+
+  return (
+    <div className="addMenuItem__container">
+      <p className="addMenuItem__title">Add Menu Item</p>
+
+      <div className="addMenuItem__inputContainer">
+        <p className="addMenuItem__inputLabel">Type</p>
+        <Select
+          className="addMenuItem__inputBody"
+          placeholder="Main Course / Side"
+          onChange={(value) => updateItem('type', value)}
+        >
+          <Option value="Main Course">Main Course</Option>
+          <Option value="Side">Side</Option>
+        </Select>
+      </div>
+
+      <div className="addMenuItem__inputContainer">
+        <p className="addMenuItem__inputLabel">Name</p>
+        <Input
+          className="addMenuItem__inputBody"
+          placeholder="Item Name"
+          onChange={(e) => updateItem('name', e.target.value)}
+        />
+      </div>
+
+      <div className="addMenuItem__inputContainer">
+        <p className="addMenuItem__inputLabel">Price</p>
+        <InputNumber
+          className="addMenuItem__inputBody"
+          placeholder="Item Price"
+          onChange={(num) => updateItem('price', num)}
+          type="number"
+        />
+      </div>
+
+      <Button disabled={!isSaveButtonEnabled} type="primary" dis>Save Item</Button>
+    </div>
   );
+};
 
 export default AddMenuItem;
