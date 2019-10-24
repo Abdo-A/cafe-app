8c31ad7652a75d964f8f1bd368c4bae0f779a6ac
Abdo <abdopoetry@gmail.com>
2019-10-24 18:52:13 +0200

    Build createMenuItem redux action

---
+++ src/store/actions/menuItemActions.js
@@ -0,0 +1,26 @@
+import * as actionTypes from './actionTypes';
+import http, { menuItemAPI } from '../../services/httpService';
+
+export const createMenuItem = (menuItem, callback) => (dispatch) => {
+  dispatch({
+    type: actionTypes.START_LOADING,
+  });
+  http
+    .post(menuItemAPI, menuItem)
+    .then(() => {
+      if (callback) callback();
+    })
+    .catch(() => {
+      dispatch({
+        type: actionTypes.SET_ERRORS,
+        payload: 'Error while creating menu item',
+      });
+    })
+    .finally(() => {
+      dispatch({
+        type: actionTypes.END_LOADING,
+      });
+    });
+};
+
+export const d = 3;
