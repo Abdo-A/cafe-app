15329c97ba38f2720414885eba884497f4712aeb
Abdo <abdopoetry@gmail.com>
2019-10-24 21:10:01 +0200

    Show a modal upon deleting a menu item

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,7 +1,7 @@
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import * as menuItemActions from '../../store/actions/menuItemActions';
 import MenuItem from './components/MenuItem';
@@ -11,6 +11,9 @@ import './style.css';
 const Menu = ({
  history, allMenuItems, getAllMenuItems, deleteMenuItem,
 }) => {
+  const [itemToBeDeletedId, setItemToBeDeletedId] = useState(false);
+  const [deleteItemModalVisible, setDeleteItemModalVisible] = useState(false);
+
   useEffect(() => {
     getAllMenuItems();
   }, [getAllMenuItems]);
@@ -20,30 +23,44 @@ const Menu = ({
   };
 
   const onDeleteItem = (id) => {
+    setDeleteItemModalVisible(true);
+    setItemToBeDeletedId(id);
+  };
+
+  const onConfirmDeleteItem = () => {
+    setDeleteItemModalVisible(false);
+
+    if (!itemToBeDeletedId) return;
     const callback = () => {
       getAllMenuItems();
+      setItemToBeDeletedId('');
     };
-    deleteMenuItem(id, callback);
+    deleteMenuItem(itemToBeDeletedId, callback);
   };
 
   return (
     <div>
-      <div>
-        <div className="menu__upperRowContainer">
-          <p className="menu__upperRowContainer__identifier">Menu</p>
-          <Button onClick={onRedirectToAddItem} type="primary">Add menu item</Button>
-        </div>
-        <div className="menu__itemsContainer">
-          {
+      <div className="menu__upperRowContainer">
+        <p className="menu__upperRowContainer__identifier">Menu</p>
+        <Button onClick={onRedirectToAddItem} type="primary">Add menu item</Button>
+      </div>
+      <div className="menu__itemsContainer">
+        {
             allMenuItems.map(({ _id: id, ...item }) => (
               <MenuItem key={id} item={item} const onDelete={() => onDeleteItem(id)} />
             ))
           }
-          {
+        {
             allMenuItems.length === 0 && <p className="menu__noItemsHint">You haven&apos;t added any Menu Items yet</p>
           }
-        </div>
       </div>
+      <Modal
+        visible={deleteItemModalVisible}
+        onOk={onConfirmDeleteItem}
+        onCancel={() => setDeleteItemModalVisible(false)}
+      >
+        <h3>Are you sure you want to delete this item?</h3>
+      </Modal>
     </div>
   );
 };
